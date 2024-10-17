09c6bab2d003cf1cb4418dc8a53666dfec086667
Hanane-hue <hananeelattar405@gmail.com>
2024-10-17 20:05:31 +0100

    First commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+
+import React from 'react';
+import './App.css';
+import Quiz from './components/Quiz';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Quiz React JS</h1>
+      <Quiz />
+    </div>
+  );
+}
+
+export default App;
